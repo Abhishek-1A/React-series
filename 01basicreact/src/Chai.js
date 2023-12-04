1b7882ef6a1c16ce96020012de5edbb65335fa13
Abhishek-A <kua77663@gmail.com>
2023-12-04 13:27:57 +0530

    create react setup for practice

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,9 @@
+function Chai () {
+    return (
+        <h2>
+            Hello from chai from react
+        </h2>
+    )
+}
+
+export default Chai;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import Chai from "./Chai";
+function App() {
+
+  return (
+  <Chai />
+  )
+}
+
+export default App
