a3bce38c895b681b8949eff2dd57c39e80eeef6b
Abhishek-A <kua77663@gmail.com>
2023-12-04 14:48:55 +0530

    create own custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
 
+function Myapp() {
+  return (
+    <div>
+      <h1>custom App !</h1>
+    </div>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Myapp />
   </React.StrictMode>,
 )
