812a5664cd64b8e86a342300b69abb9fe53cc6e7
Abhishek-A <kua77663@gmail.com>
2023-12-24 17:08:24 +0530

    created two component one is useris that store user data and another one is github that display ur github ac nd followers

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState, useSyncExternalStore } from "react";
+
+const Github = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/Abhishek-1A")
+      .then((Response) => Response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+  return (
+    <div
+      className="text-center m-4 bg-gray-600 text-white
+    p-4 text-3xl"
+    >
+      Github followes: {data.followers}
+    </div>
+  );
+};
+
+export default Github;
